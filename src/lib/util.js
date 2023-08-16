29fe54786c81a32a7822570c00816555e45fe547
yoosangoh <dhdbtkd@naver.com>
2023-08-17 01:09:35 +0900

    로그인 토큰, 쿠키 관련

--- src/lib/util.js
+++ src/lib/util.js
@@ -11,6 +11,7 @@ const Util = {
                 'Content-Type': 'application/json'
             },
             body : JSON.stringify(body_data),
+            credentials: 'include'
         })
         .then(res => res.json())
         .then(json => {
